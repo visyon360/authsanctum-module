691d71b3f3011553b6c7f87a866aa6a99f8da8cd
Daniel Carvajal <dcarvajal@kiteris.com>
2021-05-18 15:44:27 +0200

    feat(setup): User model namespaces same as starter Module

--- Http/Controllers/AuthSanctumController.php
+++ Http/Controllers/AuthSanctumController.php
@@ -2,47 +2,47 @@
 
 namespace Modules\AuthSanctum\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Modules\AuthSanctum\Http\Requests\LoginRequest;
 use Modules\AuthSanctum\Http\Requests\RegisterRequest;
+use Modules\Users\Entities\User;
 
 class AuthSanctumController extends Controller
 {
-    public function register(RegisterRequest $request)
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'password' => bcrypt($request->password),
-            'email' => $request->email
-        ]);
+	public function register(RegisterRequest $request)
+	{
+		$user = User::create([
+			'name' => $request->name,
+			'password' => bcrypt($request->password),
+			'email' => $request->email
+		]);
 
-        return response()->json([
-            'token' => $user->createToken('API Token')->plainTextToken,
-            'user' => $user
-        ]);
-    }
+		return response()->json([
+			'token' => $user->createToken('API Token')->plainTextToken,
+			'user' => $user
+		]);
+	}
 
-    public function login(LoginRequest $request)
-    {
-        if (!Auth::attempt([
-            'email' => $request->email,
-            'password' => $request->password])
-        ) {
-            return abort(401, 'Credentials not match');
-        }
+	public function login(LoginRequest $request)
+	{
+		if (!Auth::attempt([
+			'email' => $request->email,
+			'password' => $request->password])
+		) {
+			return abort(401, 'Credentials not match');
+		}
 
-        return response()->json([
-            'token' => auth()->user()->createToken('API Token')->plainTextToken,
-            'user' => auth()->user()
-        ]);
-    }
+		return response()->json([
+			'token' => auth()->user()->createToken('API Token')->plainTextToken,
+			'user' => auth()->user()
+		]);
+	}
 
-    public function logout()
-    {
-        auth()->user()->tokens()->delete();
+	public function logout()
+	{
+		auth()->user()->tokens()->delete();
 
-        return response()->json('Tokens deleted');
-    }
+		return response()->json('Tokens deleted');
+	}
 }
--- Tests/Feature/AuthSanctumControllerTest.php
+++ Tests/Feature/AuthSanctumControllerTest.php
@@ -1,53 +1,55 @@
 <?php
 namespace Modules\Authsanctum\Tests\Feature;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\Authsanctum\Tests\TestCase;
+use Modules\Users\Entities\User;
 
 class AuthSanctumControllerTest extends TestCase
 {
-    use RefreshDatabase;
-    public function test_register()
-    {
-        $response = $this->json('POST', route('api.sanctum.register'), [
-            'email' => 'test@test.dev',
-            'password' => '123456',
-            'password_confirmation' => '123456',
-            'name' => 'John Doe'
-        ])->assertOk();
+	use RefreshDatabase;
+	public function test_register()
+	{
+		$this->withExceptionHandling();
+		$response = $this->json('POST', route('api.sanctum.register'), [
+			'email' => 'test@test.dev',
+			'password' => '123456',
+			'password_confirmation' => '123456',
+			'name' => 'John Doe'
+		]);
 
-        $response->assertJsonStructure(['token', 'user']);
-    }
+		$response->assertJsonStructure(['token', 'user']);
+	}
 
-    public function test_get_auth_user()
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-        $response = $this->json('get', 'api/me')->assertStatus(200);
-        $this->assertEquals($response->decodeResponseJson()['email'], $user->email);
-    }
+	public function test_get_auth_user()
+	{
+		User::factory()->make();
+		$user = User::factory()->create();
+		$this->actingAs($user);
+		$response = $this->json('get', 'api/me')->assertStatus(200);
+		$this->assertEquals($response->decodeResponseJson()['email'], $user->email);
+	}
 
-    public function test_login()
-    {
-        $this->withExceptionHandling();
-        $user = User::factory()->create([
-            'password' => '1234'
-        ]);
-        $this->response = $this->json('post', route('api.sanctum.login', [
-            'email' => $user->email,
-            'password' => '1234'
-        ]))->assertStatus(200);
-    }
+	public function test_login()
+	{
+		$this->withExceptionHandling();
+		$user = User::factory()->create([
+			'password' => '1234'
+		]);
+		$this->response = $this->json('post', route('api.sanctum.login', [
+			'email' => $user->email,
+			'password' => '1234'
+		]))->assertStatus(200);
+	}
 
-    public function test_logout()
-    {
-        $this->withExceptionHandling();
-        $user = User::factory()->create([
-            'password' => '1234'
-        ]);
-        $this->actingAs($user);
-        $response = $this->json('post', route('api.sanctum.logout'));
-        $this->assertTrue($response->decodeResponseJson()->json == '"Tokens deleted"');
-    }
+	public function test_logout()
+	{
+		$this->withExceptionHandling();
+		$user = User::factory()->create([
+			'password' => '1234'
+		]);
+		$this->actingAs($user);
+		$response = $this->json('post', route('api.sanctum.logout'));
+		$this->assertTrue($response->decodeResponseJson()->json == '"Tokens deleted"');
+	}
 }
